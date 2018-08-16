7a2aa3630cb26b8eac5483a62d2aa8c10277dcf4
Isaiah Harris <harrisfambam@gmail.com>
2018-08-16 12:09:06 -1000

    ready to add thunk

--- kanban/src/components/App/App.js
+++ kanban/src/components/App/App.js
@@ -13,16 +13,6 @@ class App extends React.Component {
   constructor(props) {
     super(props)
 
-    this.state = {
-      titleInput: '',
-      bodyInput: '',
-      priorityInput: '',
-      statusInput: '',
-      created_byInput: '',
-      assigned_toInput: '',
-    }
-    this.addNewCard = this.addNewCard = this.addNewCard.bind(this)
-    this.handleInputChange = this.handleInputChange.bind(this);
   }
 
   componentDidMount(){
@@ -35,59 +25,6 @@ class App extends React.Component {
     })
   }
 
-  handleInputChange(event){
-    switch(event.target.id){
-      case 'title':
-      this.setState({titleInput: event.target.value})
-      break;
-      case 'body':
-      this.setState({bodyInput: event.target.value})
-      break;
-      case 'priority':
-      this.setState({priorityInput: event.target.value})
-      break;
-      case 'status':
-      this.setState({statusInput: event.target.value})
-      break;
-      case 'created_by':
-      this.setState({created_byInput: event.target.value});
-      break;
-      case 'assigned_to':
-      this.setState({assigned_toInput: event.target.value})
-      break;
-      default:
-      break;
-    }
-  }
-
-  addNewCard(event){
-    console.log('clicked', this.state.priorityInput)
-    const data = {}
-    data.title = this.state.titleInput
-    data.body = this.state.bodyInput
-    data.priority_id = this.state.priorityInput
-    data.status_id = this.state.statusInput
-    data.created_by = this.state.created_byInput
-    data.assigned_to = this.state.assigned_toInput
-
-    axios.post('/api/cards', data)
-    .then(response=>{
-      const card = response.data;
-      this.setState(prevState=>{
-        return {cards: prevState.cards.concat(card),
-          titleInput: '',
-          bodyInput: '',
-          priorityInput: '',
-          statusInput: '',
-          created_byInput: '',
-          assigned_toInput: '',
-        }
-      });
-    })
-    .catch(err=>{
-      console.log(err)
-    })
-  }
   render() {
     return (
       <div className="App">
@@ -97,16 +34,7 @@ class App extends React.Component {
         <Column status_id = {this.props.status_id} label = "In Progress" cards =  {this.props.cards}/>
         <Column status_id = {this.props.status_id} label = "Done" cards = {this.props.cards}/>
         </div>
-        <NewCardForm
-        changeHandler= {this.handleInputChange}
-        formHandler = {this.addNewCard}
-        titleInput = {this.state.titleInput}
-        bodyInput = {this.state.bodyInput}
-        priorityInput = {this.state.priorityInput}
-        statusInput = {this.state.statusInput}
-        created_byInput = {this.state.created_byInput}
-        assigned_toInput = {this.state.assigned_toInput}
-        />
+        <NewCardForm/>
       </div>
     );
   }
--- kanban/src/components/NewCardForm/index.js
+++ kanban/src/components/NewCardForm/index.js
@@ -1,64 +1,126 @@
 import React from 'react';
 import AddCardButton from '../AddCardButton';
+import { addCard } from '../../actions'
+import {connect} from 'react-redux'
+import axios from 'axios';
+class NewCardForm extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      titleInput: '',
+      bodyInput: '',
+      priorityInput: '',
+      statusInput: '',
+      created_byInput: '',
+      assigned_toInput: '',
+    }
+    this.addNewCard = this.addNewCard = this.addNewCard.bind(this)
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
 
-const NewCardForm = props => {
-  const {
-    titleInput,
-    bodyInput,
-    priorityInput,
-    statusInput,
-    created_byInput,
-    assigned_toInput,
-    formHandler,
-    changeHandler
-  } = props
+  handleInputChange(event){
+    switch(event.target.id){
+      case 'title':
+      this.setState({titleInput: event.target.value})
+      break;
+      case 'body':
+      this.setState({bodyInput: event.target.value})
+      break;
+      case 'priority':
+      this.setState({priorityInput: event.target.value})
+      break;
+      case 'status':
+      this.setState({statusInput: event.target.value})
+      break;
+      case 'created_by':
+      this.setState({created_byInput: event.target.value});
+      break;
+      case 'assigned_to':
+      this.setState({assigned_toInput: event.target.value})
+      break;
+      default:
+      break;
+    }
+  }
+  addNewCard(event){
+    console.log('clicked', this.state.priorityInput)
+    const data = {}
+    data.title = this.state.titleInput
+    data.body = this.state.bodyInput
+    data.priority_id = this.state.priorityInput
+    data.status_id = this.state.statusInput
+    data.created_by = this.state.created_byInput
+    data.assigned_to = this.state.assigned_toInput
+
+    axios.post('/api/cards', data)
+    .then(response=>{
+      const card = response.data;
+      this.setState(prevState=>{
+        return {cards: prevState.cards.concat(card),
+          titleInput: '',
+          bodyInput: '',
+          priorityInput: '',
+          statusInput: '',
+          created_byInput: '',
+          assigned_toInput: '',
+        }
+      });
+    })
+    .catch(err=>{
+      console.log(err)
+    })
+  }
+
+render(){
   return (
     <div className="NewCardForm-container">
       <label htmlFor="title">Title:</label>
       <input type="text"
         name="title"
         id="title"
-        value={titleInput}
-        onChange={changeHandler}
+        value={this.state.titleInput}
+        onChange={this.handleInputChange}
       />
       <label htmlFor="body">Body:</label>
       <input type="text"
         name="body"
         id="body"
-        value={bodyInput}
-        onChange={changeHandler}
+        value={this.state.bodyInput}
+        onChange={this.handleInputChange}
       />
       <label htmlFor="priority">Priority_id:</label>
       <input type="text"
         name="priority_id"
         id="priority"
-        value={priorityInput}
-        onChange={changeHandler}
+        value={this.state.priorityInput}
+        onChange={this.handleInputChange}
       />
       <label htmlFor="status">Status_id:</label>
       <input type="text"
         name="status_id"
         id="status"
-        value={statusInput}
-        onChange={changeHandler}
+        value={this.state.statusInput}
+        onChange={this.handleInputChange}
       />
       <label htmlFor="created_by">Created_by:</label>
       <input type="text"
         name="created_by"
         id="created_by"
-        value={created_byInput}
-        onChange={changeHandler}
+        value={this.state.created_byInput}
+        onChange={this.handleInputChange}
       />
       <label htmlFor="assigned_to">Assigned_to:</label>
       <input type="text"
         name="assigned_to"
         id="assigned_to"
-        value={assigned_toInput}
-        onChange={changeHandler}
+        value={this.state.assigned_toInput}
+        onChange={this.handleInputChange}
       />
-      <AddCardButton label = 'Add Task' clickHandler = {formHandler}/>
+      <AddCardButton label = 'Add Task' clickHandler = {this.addNewCard}/>
     </div>
   )
+}
+
 }
 
 export default NewCardForm;
