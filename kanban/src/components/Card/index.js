da20382ab6d141c7ab0141de58d265bcd70ebe30
Isaiah Harris <harrisfambam@gmail.com>
2018-08-17 10:34:29 -1000

    get button to show

--- kanban/src/components/Card/index.js
+++ kanban/src/components/Card/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../../styles.css'
 import PropTypes from 'prop-types';
-
+import EditCardButton from '../EditCardButton';
 const Card = ({title, body, statusName, priority, status_id, created_by, assigned_to, label})=>(
   
 
@@ -9,8 +9,16 @@ const Card = ({title, body, statusName, priority, status_id, created_by, assigne
       <div className="title">Title: { title }</div>    
       <div className="body">Body: { body }</div>
       <div className="priority_id">Priority: { priority }</div>
-      <div className="created_by">Created By { created_by }</div>
-      <div className="assigned_to">Assigned To{ assigned_to }</div> 
+      <div className="created_by">Created By: { created_by }</div>
+      <div className="assigned_to">Assigned To: { assigned_to }</div> 
+      <EditCardButton
+       statusName = {statusName}
+       title = {title}
+       body = {body}
+       priority = {priority}
+       created_by = {created_by}
+       assigned_to = {assigned_to}
+       />
       </div> 
 
 
---
+++ kanban/src/components/EditCardButton/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './EditCardButton.css';
+
+const EditCardButton = props=>{
+  return (
+    <button>hi</button>
+  )
+}
+
+export default EditCardButton;
